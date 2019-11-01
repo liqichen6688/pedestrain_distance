39756c74792db8a6542667bba2a71101ae0ad7ac
liqichen <liqichen@me.com>
2019-11-01 22:53:23 +0800

    gogo

--- producersetting/kafka_setting.go
+++ producersetting/kafka_setting.go
@@ -6,7 +6,6 @@ import (
 	cluster "github.com/bsm/sarama-cluster"
 	log "github.com/sirupsen/logrus"
 	"gitlab.sz.sensetime.com/rd-platform/public/strategy-service/utils/kafka"
-	"pedestrain_distance/Main"
 	"time"
 )
 
@@ -23,7 +22,7 @@ func NewProducer(config *ProducerConfig) (*kafka.Producer, error) {
 	return *producer, err
 }
 
-func ChannelToKafka(p *kafka.Producer, c chan Main.DistanceTimeStamp, topic string, retry int) error {
+func ChannelToKafka(p *kafka.Producer, c chan interface{}, topic string, retry int) error {
 	var err error
 	for timestamp := range c {
 		respite := time.Duration(1) * time.Second
